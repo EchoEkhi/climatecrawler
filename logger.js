9446e8649fe0d10742b715bcd0693ee62e87280b
EchoEkhi <echoekhi@gmail.com>
2021-09-30 16:15:59 +0100

    Dirname fix

--- logger.js
+++ logger.js
@@ -6,11 +6,11 @@ const log = winston.createLogger({
         winston.format.printf(({ level, message, timestamp }) => `[${level}]\t[${timestamp}]\t${message}`)
     ),
     transports: [
-        new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
-        new winston.transports.File({ filename: 'logs/warn.log', level: 'warn' }),
-        new winston.transports.File({ filename: 'logs/info.log', level: 'info' }),
+        new winston.transports.File({ filename: __dirname + '/logs/error.log', level: 'error' }),
+        new winston.transports.File({ filename: __dirname + '/logs/warn.log', level: 'warn' }),
+        new winston.transports.File({ filename: __dirname + '/logs/info.log', level: 'info' }),
         new winston.transports.Console({ level: 'info' }),
-        new winston.transports.File({ filename: 'logs/combined.log' })
+        new winston.transports.File({ filename: __dirname + '/logs/combined.log' })
     ]
 })
 
